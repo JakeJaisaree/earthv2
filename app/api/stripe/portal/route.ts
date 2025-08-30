ac653afce7beabd13a0db8998c38fab41d475b18
JakeJaisaree <JakesAILibrary@gmail.com>
2025-08-30 00:55:02 -0500

    Update route.ts

--- app/api/stripe/portal/route.ts
+++ app/api/stripe/portal/route.ts
@@ -1,16 +1,17 @@
 import Stripe from "stripe";
+import { cookies } from "next/headers";
 export const runtime = "nodejs";
 
 export async function POST() {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const customerId = cookies().get("cus_Sxbk87a6sbCQyT")?.value;
+  if (!customerId) return new Response("No customer", { status: 401 });
 
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
   const portal = await stripe.billingPortal.sessions.create({
-    customer: "cus_Sxbk87a6sbCQyT", 
+    customer: customerId,
     return_url: `${process.env.NEXT_PUBLIC_APP_URL}/account`,
   });
 
   return Response.json({ url: portal.url });
 }
-
-
 
