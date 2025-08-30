63473fb3edeff3e76f0ab8d4c356fecf4fff7a9d
JakeJaisaree <JakesAILibrary@gmail.com>
2025-08-30 00:46:59 -0500

    Update route.ts

--- app/api/stripe/portal/route.ts
+++ app/api/stripe/portal/route.ts
@@ -5,11 +5,12 @@ export async function POST() {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
   const portal = await stripe.billingPortal.sessions.create({
-    customer: "Jack Wild", 
+    customer: "cus_Sxbk87a6sbCQyT", 
     return_url: `${process.env.NEXT_PUBLIC_APP_URL}/account`,
   });
 
   return Response.json({ url: portal.url });
 }
+
 
 
